97e427eae55f5f3185fb5f0fddf252ecf975385a
Suyunmeng <Susus0175@proton.me>
2025-10-21 21:46:46 +0800

    refactor(scripts): remove redundant comments in update_pubspec_version.sh and simplify the version line in pubspec.yaml

--- .github/scripts/update_pubspec_version.sh
+++ .github/scripts/update_pubspec_version.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 更新pubspec.yaml中的版本号以与OpenList主程序版本同步
-# pubspec.yaml使用三段式版本号，Android版本号会自动添加时间戳
-
 VERSION_FILE="$GITHUB_WORKSPACE/openlist_version"
 PUBSPEC_FILE="$GITHUB_WORKSPACE/pubspec.yaml"
 
@@ -16,16 +13,12 @@ if [ ! -f "$PUBSPEC_FILE" ]; then
     exit 1
 fi
 
-# 读取OpenList版本号
 OPENLIST_VERSION=$(cat "$VERSION_FILE")
 
-# 去掉版本号前缀'v'
 BASE_VERSION=${OPENLIST_VERSION#v}
 
 echo "Updating pubspec.yaml version to: $BASE_VERSION"
 
-# 使用sed更新pubspec.yaml中的版本号
-# 保持build number为+1，只更新版本号部分（三段式）
-sed -i "s/^version: [0-9]\+\.[0-9]\+\.[0-9]\++[0-9]\+.*/version: ${BASE_VERSION}+1 # 与OpenList主程序版本同步，Android版本号自动添加时间戳/" "$PUBSPEC_FILE"
+sed -i "s/^version: [0-9]\+\.[0-9]\+\.[0-9]\++[0-9]\+.*/version: ${BASE_VERSION}+1/" "$PUBSPEC_FILE"
 
 echo "pubspec.yaml version updated successfully"
